c5178d26eddbd75efd0b3cca0a90928b9ec57bce
Salman Abdulqadir <103096886+Salman-Abdulqadir@users.noreply.github.com>
2022-12-17 21:31:16 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 //router
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 //redux setup
 import { Provider } from "react-redux";
 import store from "./redux/store";
@@ -13,11 +13,12 @@ import store from "./redux/store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 reportWebVitals();
+
